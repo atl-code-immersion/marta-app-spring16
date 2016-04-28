766adbc226367cb7771f4be45c30e72de1ea0364
Aaron Groch <ar.groch@gmail.com>
2016-04-28 08:38:24 -0400

    added new controller and view to track trains

---
+++ app/helpers/marta_helper.rb
@@ -0,0 +1,2 @@
+module MartaHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
 
-  root 'locations#new'
+  get 'trains' => 'marta#index'
+  post 'trains' => 'marta#index'
 
+  root 'locations#new'
+ 
   resources :locations
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
